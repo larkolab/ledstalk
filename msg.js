b2778d87dd48c565bde52124c344ecf48e44ca6f
Mick <mick.coracin@gmail.com>
2014-11-09 16:28:25 +0100

    Created buttons, changed style

--- msg.js
+++ msg.js
@@ -1,9 +1,10 @@
-var feedID        = 144422134;             // Feed ID (the last number on the URL on the feed page on Xively)
-var message_datastreamID  = "MESSAGE_CHANNEL";     // Datastream ID
-var presence_datastreamID  = "PRESENCE_CHANNEL";     // Datastream ID
-var selector      = "#presence_element";   // Your element on the page - takes any valid jQuery selector
+// See http://xively.github.io/xively-js/tutorial/ for details
 
-// Set the Xively API key (https://xively.com/users/YOUR_USERNAME/keys)
+var feedID        = 144422134;
+var message_datastreamID  = "MESSAGE_CHANNEL";
+var presence_datastreamID  = "PRESENCE_CHANNEL";
+var temperature_datastreamID = "TEMPERATURE_CHANNEL";
+
 xively.setKey( "Y8bnZpN22M05lpN3qlFd26JNJKzoXCc4Ppmf7NKExv9bQecw" );
 
 // Set datastream data to Xively when button is pressed
@@ -11,27 +12,37 @@ function sendMessage() {
   // Get message from input text box
   var message = document.getElementById('message_id').value;
 
-  //alert("send: " + message);
   xively.datastream.update(feedID, message_datastreamID, {current_value: message}, function(data){ alert("channel updated");})
 }
 
 // Make sure the document is ready to be handled
 $(document).ready(function($) {
-  // Get datastream data from Xively
+  //
+  // Get PRESENCE datastream data from Xively
+  //
   xively.datastream.get (feedID, presence_datastreamID, function ( datastream ) {
-    // WARNING: This code is only executed when we get a response back from Xively, it will likely execute after the rest your script
-    // NOTE: The variable "datastream" will contain all the Datastream information as an object. The structure of Datastream objects can be found at: 
-    // https://xively.com/dev/docs/api/quick_reference/api_resource_attributes/#datastream
-
     // Display the current value from the datastream
-    $(selector).html( datastream["current_value"] );
+    $("#presence_element").html( datastream["current_value"] );
 
-    // Getting realtime! The function associated with the subscribe method will be executed every time there is an update to the datastream
+    // Getting realtime!
     xively.datastream.subscribe( feedID, presence_datastreamID, function ( event , datastream_updated ) {
       // Display the current value from the updated datastream
-      $(selector).html( datastream_updated["current_value"] );
+      $("#presence_element").html( datastream_updated["current_value"] );
     });
   });
 
-  // WARNING: Code here will continue executing while we get the datastream data from Xively, use the function associated with datastream.get to work with the data, when the request is complete
+  //
+  // Get TEMPERATURE datastream data from Xively
+  //
+  xively.datastream.get (feedID, temperature_datastreamID, function ( datastream ) {
+    // Display the current value from the datastream
+    $("#temperature_element").html( datastream["current_value"] );
+
+    // Getting realtime!
+    xively.datastream.subscribe( feedID, temperature_datastreamID, function ( event , datastream_updated ) {
+      // Display the current value from the updated datastream
+      $("#temperature_element").html( datastream_updated["current_value"] );
+    });
+  });
+
 });
